f12651ae60ac6f80ba79c9328bd0eab54459259b
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-11 06:51:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves(array)
-  array.each_with_index { |value, index| array[value] = index }
+   i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
 end
 
 def summon_captain_planet# code an argument here
