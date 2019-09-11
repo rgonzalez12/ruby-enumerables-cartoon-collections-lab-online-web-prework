b9f5aca61ca3bfc50b51a0a60c9cd534d46f1542
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-11 06:54:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def summon_captain_planet(array)
   nu_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
    i = 0 
   if  array.any? {|i| i.length > 4}
     return true
@@ -26,7 +26,7 @@ def long_planeteer_calls# code an argument here
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
