94caeb75bd50f6c114a77663bf347693d3a64ebe
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-11 06:55:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,4 +29,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
